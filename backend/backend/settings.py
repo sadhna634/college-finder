a7c7010237915c6f7b1414b79e6b1d6ce032e66f
Sadhna Choudhary <sadhnachoudharys797@gmail.com>
2026-04-29 10:26:39 +0530

    added static root

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+SECRET_KEY = 'django-insecure-key'
